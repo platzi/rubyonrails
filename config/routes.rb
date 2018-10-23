93d425f051e36e85500c20efb3ddb41c29e97164
Karen Chizón <karen.chizon@gmail.com>
2018-10-23 18:07:01 -0500

    update: dynamic view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/create'
+  get 'articles/update'
+  get 'articles/destroy'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#index'
