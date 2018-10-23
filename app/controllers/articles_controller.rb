e7ee51f6dd78e77b2ccfa6c36d98f98954ddc275
Karen Chizón <karen.chizon@gmail.com>
2018-10-23 18:29:57 -0500

    update: dynamicviews

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -7,17 +9,51 @@ class ArticlesController < ApplicationController
   end
 
   def new
+    @article = Article.new
   end
 
   def edit
   end
 
   def create
+    @article = Article.new(article_params)
+    respond_to do |format|
+      if @article.save
+        format.html { redirect_to @article, notice: 'Tu artículo ha sido publicado.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @article.update(article_params)
+        format.html { redirect_to @article, notice: 'Tu artículo ha sido publicado.' }
+        format.json { render :show, status: :created, location: @article }
+      else
+        format.html { render :new }
+        format.json { render json: @article.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @article.destroy
+    respond_to do |format|
+      format.html { redirect_to articles_url, notice: 'Tu artículo ha sido eliminado.' }
+      format.json { head :no_content }
+    end
   end
+
+  private
+    def set_article
+      @article = Article.find(params[:id])
+    end
+
+    def article_params
+      params.require(:article).permit(:title, :body, :user_id)
+    end
 end
